2ada9684e63cd4e713c65b0c1e0e0ff2e88988e8
Jonas Enge <jonas.enge@gmail.com>
2018-06-25 13:58:09 +0200

    Genesis commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: 'https://api.lifx.com/v1/lights/all/state',
+  url: 'https://api.lifx.com/v1/lights/all/state', // See https://api.developer.lifx.com/v1/docs/selectors to control selected lights
   token: 'your-token',
   serviceUrl: 'https://s3.eu-central-1.amazonaws.com/luftstatus/areas.json',
   area: 'Grenland',
