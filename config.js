b497ef434498d91e6d08ae4e2793c890defa255d
Jonas Enge <jonas.enge@gmail.com>
2018-06-25 13:46:43 +0200

    Genesis commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   url: 'https://api.lifx.com/v1/lights/all/state',
-  token: 'c13a3b8dfb37cf2944efc21b52889104dff208bc20d1190d6f75c07165813adc',
+  token: 'your-token',
   serviceUrl: 'https://s3.eu-central-1.amazonaws.com/luftstatus/areas.json',
   area: 'Grenland',
   pollIntervalMinutes: 60
